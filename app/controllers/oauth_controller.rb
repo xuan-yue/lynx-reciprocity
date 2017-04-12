9e3dc2f214c18b8905caee4bb581cbf8f496ced3
Bruno Oliveira Paes de Andrade <boliveir@mail.sfsu.edu>
2017-04-12 16:30:40 -0700

    oauth management, added facebook login

--- app/controllers/oauth_controller.rb
+++ app/controllers/oauth_controller.rb
@@ -1,19 +1,6 @@
 class OauthController < ApplicationController
 
-  def callback
-    begin
-        if request.env['provider'] == "linkedin"
-            linkedin_callback
-            user = User.find_by(email: request.env['omniauth.auth'][:info][:email])
-            redirect_to user
-        end
-    rescue => e
-      flash[:alert] = "There was an error while trying to authenticate your account."
-      redirect_to register_path
-    end
-  end
-
-  def linkedin_callback
+ def callback 
     begin
         user = User.find_by(email: request.env['omniauth.auth'][:info][:email])
         if user && !user.activated?
@@ -28,18 +15,20 @@ class OauthController < ApplicationController
             @user.activate
             user = @user
         end
-         linkedin = LinkedinProfile.find_by(email: user[:email])
-        if !linkedin
-            @linkedin = LinkedinProfile.new
-            @linkedin[:email] = user[:email]
-            @linkedin[:image_url] = request.env['omniauth.auth'][:info][:image]
-            @linkedin[:profile_url] = request.env['omniauth.auth'][:info][:urls][:public_profile]
-            @linkedin[:raw_data] = request.env['omniauth.auth'][:extra][:raw_info].to_json
-            @linkedin.save
+        if request.env['omniauth.auth'][:provider] == "linkedin"
+            linkedin = LinkedinProfile.find_by(email: user[:email])
+            if !linkedin
+                @linkedin = LinkedinProfile.new
+                @linkedin[:email] = user[:email]
+                @linkedin[:image_url] = request.env['omniauth.auth'][:info][:image]
+                @linkedin[:profile_url] = request.env['omniauth.auth'][:info][:urls][:public_profile]
+                @linkedin[:raw_data] = request.env['omniauth.auth'][:extra][:raw_info].to_json
+                @linkedin.save
+            end
         end
-       log_in user
+        log_in user
         redirect_to user
-     rescue => e
+    rescue => e
       flash[:alert] = "uncaught #{e} exception while handling connection: #{e.message}"
       redirect_to register_path
     end
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :linkedin, '86tcgx3lqy8txp', '2CO3cJJZKolfxrDG'
+  provider :facebook, '1943304002565965', '23fb433db9dd3e77694648933bf9485e', scope: 'email', info_fields: 'email, first_name, last_name'
 end
